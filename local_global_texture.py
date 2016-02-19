9986160815482171e43aa652f1fdfe82cf7c874b
Victor Lavrentev <victor.lavrentev@gmail.com>
2016-02-19 16:00:46 +0200

    Create local_global_texture.py

---
+++ local_global_texture.py
@@ -0,0 +1,5 @@
+hou.hscript("setenv {0}={1}".format('TEXTUREPATH','/home/victor/Documents/PointsOfPower/texture'))
+hou.ui.displayMessage("Local texture")
+
+hou.hscript("setenv {0}={1}".format('TEXTUREPATH', '/mnt/projects/PointsOfPower/film/assetBuilds/Character/Unkas/texturing/texturing/0000'))
+hou.ui.displayMessage("Global texture")
