34ffe27f176de3f54b5f9ed4947c56247214fbca
Victor Lavrentev <victor.lavrentev@gmail.com>
2017-11-10 01:01:54 +0200

    Create xml_Alexa.py
    Alexa

---
+++ xml_Alexa.py
@@ -0,0 +1,11 @@
+import xml.etree.cElementTree as ET
+tree = ET.ElementTree(file='/mnt/sda5/shrimo/Projects/ddog/heli_0010/A013C002_170628_R6Y8.xml')
+root = tree.getroot()
+
+for _xml in root.findall('PropertyStrings'):
+    nameLT = _xml.find('LensType')
+    nameAR = _xml.find('AlexaReel')
+    nameWB = _xml.find('WhiteBalance')
+    print nameLT.text, nameAR.text,nameWB.text,
+
+nuke.nodes.StickyNote(label='LensType -'+nameLT.text+'\n'+" Reel - "+nameAR.text++'\n'+" WB - "+nameWB.text)
