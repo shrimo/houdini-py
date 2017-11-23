772d59508bfd492ce94c1d728a547f7e0bd770ab
Victor Lavrentev <victor.lavrentev@gmail.com>
2017-11-23 03:41:31 +0200

    Create arriMetaData.py

---
+++ arriMetaData.py
@@ -0,0 +1,44 @@
+import os
+import math
+import re
+
+selectedNode = nuke.selectedNode()
+nodeName = selectedNode.name()
+node = nuke.toNode(nodeName)
+if nuke.getNodeClassName(node) != 'Read':
+    nuke.message('Please select a read Node')
+    print 'Please select a read Node'
+
+metaData = node.metadata()
+
+FD = metaData['exr/FocusDistance']
+act = metaData['exr/LensModel']
+
+first = node.firstFrame()
+last = node.lastFrame()
+#ret = nuke.getFramesAndViews( 'Create Camera from Metadata', '%s-%s' %( first, last )  )
+#frameRange = nuke.FrameRange( ret[0] )
+#camViews = (ret[1])
+
+#print FD, frameRange
+
+#FD_num = re.findall(r"[-+]?\d*\.\d+|\d+",metaData['exr/FocusDistance'])
+
+#print FD_num
+
+cam = nuke.nodes.Camera (name="Camera %s" % act)
+
+cam['focal_point'].setAnimated()
+
+
+f = open('/home/shrimo/Documents/python/data.txt', 'a')
+
+for index in range(0,300):
+    frame = index
+    nuke.activeViewer().frameControl(frame)
+    FD_num = re.findall(r"[-+]?\d*\.\d+|\d+",node.metadata('exr/FocusDistance',index))
+    print frame, FD_num[0]
+    f.write(str(FD_num))	
+    cam['focal_point'].setValueAt(float(FD_num[0]), frame)
+
+f.close()
